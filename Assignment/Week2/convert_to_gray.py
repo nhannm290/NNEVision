d51c9372661423922f19e23ca4cb38c483df4ab9
18521165 <80077471+18521165@users.noreply.github.com>
2021-03-16 10:02:42 +0700

    Update convert_to_gray.py

--- Assignment/Week2/convert_to_gray.py
+++ Assignment/Week2/convert_to_gray.py
@@ -9,9 +9,9 @@ cv2.waitKey()
 
 # convert gray image
 gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
+gray_img_resize = cv2.resize(gray_image,(500,500),interpolation=cv2.INTER_AREA)
 # save to new file
-cv2.imwrite('messi_gray.png',gray_image)
-cv2.imshow("messi_gray", gray_image)
+cv2.imwrite('messi_gray.png',gray_img_resize)
+cv2.imshow("messi_gray", gray_img_resize)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
