251d6e28a8d88ac0a94747844d013e17259babb4
joenguyen309 <74529480+joenguyen309@users.noreply.github.com>
2021-03-26 08:16:54 +0700

    Update Imange_Error.py

--- Assignment/Week2/Python/Convert/Imange_Error.py
+++ Assignment/Week2/Python/Convert/Imange_Error.py
@@ -1,19 +1,32 @@
 import cv2 as cv
 import numpy as np
 
-image_blank = np.zeros((533,800),dtype=float)
+f_detail = open("Detail.txt","r")
+imaga_width = int(f_detail.readline())
+image_height = int(f_detail.readline())
+
+image_blank = np.zeros((image_height,imaga_width,3),np.float32)
 
 f_True_Imange = open("Gray_Hex.txt","r")
 f_Error_Image = open("bitmap.out","r")
+f_result = open("result.txt","w")
 
-for i in range(533):
-    for j in range(800):
-        data_true = f_True_Imange.readline()
-        data_error = f_Error_Image.readline()
-        int_data_true = int(data_true,16)
-        int_data_error = int(data_error,16)
-        image_blank[i][j] = float(int_data_true-int_data_error)/255
+f1_line = f_True_Imange.readline()
+f2_line = f_Error_Image.readline()
+
+pixel = []
+
+while f1_line != '' or f2_line != '':
+    pixel.append((int(f1_line,16) - int(f2_line,16))*255)
+    f_result.write(str(int(f1_line,16) - int(f2_line,16)) +"\n")
+    f1_line = f_True_Imange.readline()
+    f2_line = f_Error_Image.readline()
+k=0
+for i in range(image_height):
+    for j in range (imaga_width):
+        image_blank[i][j] = (pixel[k],pixel[k],pixel[k])
+        k = k +1
+
 cv.imwrite("Image_Error_Check.png",image_blank)
-cv.imshow('Image_Sult',image_blank)
 cv.waitKey(0)
 cv.destroyAllWindows()
