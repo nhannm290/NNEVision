7f32eb7e018d126c0ccc662dc077710fea1308ff
joenguyen309 <74529480+joenguyen309@users.noreply.github.com>
2021-03-28 16:23:13 +0700

    Update controller.v

--- Assignment/Week2/FPU/FMU_Pipeline/controller.v
+++ Assignment/Week2/FPU/FMU_Pipeline/controller.v
@@ -3,76 +3,62 @@ input clk,start,
 output reg flag,
 output reg NEG1,MAN3,OUT4,XOR4,DONE5,REG2,REG4
 ); 
-reg [2:0] nextstate;
-	parameter S0 = 3'b000;
-	parameter S1 = 3'b001;
-	parameter S2 = 3'b010;
-	parameter S3 = 3'b011;
-	parameter S4 = 3'b100;
-	always @(posedge clk)begin
-		case(nextstate)
-			S0:nextstate <= S1;
-			S1:if(start ==1)nextstate <= S2;
-			S2:nextstate <= S3;
-			S3:nextstate <= S4;
-			default: nextstate <= S1;
-		endcase
+
+/////////////////////////////////STATE0///////////////////////////////////////////////////////////////////////////////////////
+//////////////Reg For STATE0//////////////////////
+	reg REG2_0,MAN3_0,OUT4_0,XOR4_0,REG4_0,DONE5_0,flag_0;
+
+	always @(posedge clk ) begin
+		if (start ==1) begin
+			NEG1 <= 1'b1;
+			REG2_0 <= 1'b1;
+			MAN3_0 <= 1'b1;
+			OUT4_0 <= 1'b1;
+			XOR4_0 <= 1'b1;
+			REG4_0 <= 1'b1;
+			DONE5_0 <= 1'b1;
+			flag_0 <= 1'b1;
+		end
+		else begin
+			NEG1 <= 1'b0;
+			REG2_0 <= 1'b0;
+			MAN3_0 <= 1'b0;
+			OUT4_0 <= 1'b0;
+			XOR4_0 <= 1'b0;
+			REG4_0 <= 1'b0;
+			DONE5_0 <= 1'b0;
+			flag_0 <= 1'b0;
+		end
 	end
-	always @(nextstate)begin
-		case(nextstate)
-		S0:begin
-			NEG1 = 1'b0;
-			REG2 = 1'b0;
-			MAN3 = 1'b0;
-			OUT4 = 1'b0;
-			XOR4 = 1'b0;
-			REG4 = 1'b0;
-			DONE5 = 1'b0;
-			flag =1'b0;
-		end
-		S1:begin
-			NEG1 = 1'b1;
-			REG2 = 1'b0;
-			MAN3 = 1'b0;
-			OUT4 = 1'b0;
-			XOR4 = 1'b0;
-			REG4 = 1'b0;
-			DONE5 = 1'b0;
-			flag =1'b0;
-		end
-		S2:begin
-			NEG1 = 1'b0;
-			REG2 = 1'b1;
-			MAN3 = 1'b0;
-			OUT4 = 1'b0;
-			XOR4 = 1'b0;
-			REG4 = 1'b0;
-			DONE5 = 1'b0;
-			flag =1'b0;
-		end
-		S3:begin
-			NEG1 = 1'b0;
-			REG2 = 1'b0;
-			MAN3 = 1'b1;
-			OUT4 = 1'b1;
-			XOR4 = 1'b1;
-			REG4 = 1'b1;
-			DONE5 = 1'b0;
-			flag =1'b0;
-		end
-		S4:begin
-			NEG1 = 1'b0;
-			REG2 = 1'b0;
-			MAN3 = 1'b0;
-			OUT4 = 1'b0;
-			XOR4 = 1'b0;
-			REG4 = 1'b0;
-			DONE5 = 1'b1;
-			flag =1'b1;
-		end
-		
-	//ADD1,NEG1,ADD2,MUL2,MAN3,OUT4,DONE5
-		default:nextstate = S1;
-	endcase
+/////////////////////////////////STATE1///////////////////////////////////////////////////////////////////////////////////////
+//////////////Reg For STATE1//////////////////////
+	reg MAN3_1,OUT4_1,XOR4_1,REG4_1,DONE5_1,flag_1;
+	always @(posedge clk ) begin
+			REG2 	<= REG2_0;
+			MAN3_1 	<= MAN3_0;
+			OUT4_1 	<= OUT4_0;
+			XOR4_1 	<= XOR4_0;
+			REG4_1 	<= REG4_0;
+			DONE5_1 <= DONE5_0;
+			flag_1 	<= flag_0;
 	end
+/////////////////////////////////STATE2///////////////////////////////////////////////////////////////////////////////////////
+//////////////Reg For STATE2//////////////////////
+	reg DONE5_2,flag_2;
+	always @(posedge clk ) begin
+			MAN3	<= MAN3_1;
+			OUT4	<= OUT4_1;
+			XOR4 	<= XOR4_1;
+			REG4 	<= REG4_1;
+			DONE5_2	<= DONE5_1;
+			flag_2 	<= flag_1;
+	end
+/////////////////////////////////STATE3///////////////////////////////////////////////////////////////////////////////////////
+//////////////Reg For STATE3//////////////////////
+	always @(posedge clk ) begin
+			DONE5	<= DONE5_2;
+			flag 	<= flag_2;
+	end
+
+
 endmodule
