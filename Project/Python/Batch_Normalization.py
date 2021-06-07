1bc7ad0ba2f7e385132eb992600574f382d3579b
joenguyen309 <hl11092000@gmail.com>
2021-06-07 20:14:13 +0700

    Add testbench for 4 module

--- Project/Python/Batch_Normalization.py
+++ Project/Python/Batch_Normalization.py
@@ -5,5 +5,13 @@ Beta_Address = ""
 Moving_Mean_Address = ""
 Moving_Variance_Address = ""
 
+A_Address = ""
+B_Address = ""
+
 file_Gama = open(Gama_Address,"r")
 file_Beta = open(Beta_Address,"r")
+file_Mean = open(Moving_Mean_Address,"r")
+file_Variance = open(Moving_Variance_Address, "r")
+
+file_A = open(A_Address,"w")
+file_B = open(B_Address,"w")
