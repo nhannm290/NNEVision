b0900f6e21403b1e5b69707db4655ddee5aede15
joenguyen309 <hl11092000@gmail.com>
2021-06-07 09:43:41 +0700

    Complete module

--- Project/RTL/MaxPooling/Find_Max_3x3.v
+++ Project/RTL/MaxPooling/Find_Max_3x3.v
@@ -14,6 +14,20 @@ module Find_Max_3x3 (
     output [31:0] Data_Out,
     output Valid_Out
 );
+    //Controler
+    assign Valid_Out= (Counter ==4 && Valid_In ==1)? 1'd1:1'd0;
+    always @(posedge clk or negedge rst) begin
+        if(~rst) begin 
+            Counter <=0;
+        end
+        else if (Valid_In ==1 && Counter !=4) Counter <= Counter +1'd1;
+        else if (Valid_In ==1 && Counter ==4) Counter <= Counter;
+        else if (Valid_In == 0 && Counter ==4) Counter <=0;
+    end
+
+
+
+
     // 1F Pipeline
     wire [31:0] sub0_Data_Out0,sub0_Data_Out1,sub0_Data_Out2,sub0_Data_Out3;
 
@@ -115,5 +129,37 @@ module Find_Max_3x3 (
             .Data_Out({reg3_Data_Out0,reg3_Data_Out1})
         );
     // 4F Pipeline
+
+    wire [31:0] sub3_Data_Out;
+    wire sub3_Valid_Out;
+
+    FP_Adder sub3 (
+        .Data_A(reg3_Data_Out0),
+        .Data_B(reg3_Data_Out1),
+        .Valid_In(Valid_In),
+        .Mode(1'b1),
+        .RMode(2'b0),
+        .Data_Out(sub3_Data_Out),
+        .Valid_Out(sub3_Valid_Out)
+    );
+    
+    wire [31:0] mux3_Data_Out;
+
+    Mux_nbit #(.DATA_WIDHT(32)) 
+        mux3(
+            .Data_A(reg3_Data_Out0),
+            .Data_B(reg3_Data_Out1),
+            .Select(sub3_Data_Out[31]),
+            .Data_Out(mux3_Data_Out)
+        );
+
+    nbit_Dff #(.DATA_WIDHT(32)) 
+        reg4 (
+            .Data_In(mux3_Data_Out),
+            .clk(clk),
+            .rst(rst),
+            .enable(1'b1),
+            .Data_Out(Data_Out)
+        );
 
 endmodule
