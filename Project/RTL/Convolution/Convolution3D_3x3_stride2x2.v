aa7f2214c6272d256ef14f8bf8a8c1dd6bc9054a
joenguyen309 <hl11092000@gmail.com>
2021-06-08 13:42:11 +0700

    structure module

--- Project/RTL/Convolution/Convolution3D_3x3_stride2x2.v
+++ Project/RTL/Convolution/Convolution3D_3x3_stride2x2.v
@@ -1,11 +1,44 @@
-module Convolution3D_3x3_stride2x2 #(parameter IMG_WIDHT = 299,parameter IMG_HEIGHT = 299) (
-    input [31:0] RED.
-    input [31:0] GREEN,
-    input [31:0] BLUE
+module Convolution3D_3x3_stride2x2_3_Channel #(parameter IMG_WIDHT = 299,parameter IMG_HEIGHT = 299) (
+    input [31:0] Channel0,
+    input [31:0] Channel1,
+    input [31:0] Channel2,
+    input [31:0] Kernel0,
+    input [31:0] Kernel1,
+    input [31:0] Kernel2,
+    input [31:0] Kernel3,
+    input [31:0] Kernel4,
+    input [31:0] Kernel5,
+    input [31:0] Kernel6,
+    input [31:0] Kernel7,
+    input [31:0] Kernel8,
+    input [31:0] Kernel9,
+    input [31:0] Kernel10,
+    input [31:0] Kernel11,
+    input [31:0] Kernel12,
+    input [31:0] Kernel13,
+    input [31:0] Kernel14,
+    input [31:0] Kernel15,
+    input [31:0] Kernel16,
+    input [31:0] Kernel17,
+    input [31:0] Kernel18,
+    input [31:0] Kernel19,
+    input [31:0] Kernel20,
+    input [31:0] Kernel21,
+    input [31:0] Kernel22,
+    input [31:0] Kernel23,
+    input [31:0] Kernel24,
+    input [31:0] Kernel25,
+    input [31:0] Kernel26,
+    input Valid_In,
+    input clk,
+    input rst,
+    output [31:0] Data_Out,
+    output Valid_Out
+
 );
     Convolution2D_3x3_stride2x2  #(.IMG_WIDHT(IMG_WIDHT),.IMG_HEIGHT(IMG_WIDHT))
         conv2d_RED(
-            .Data_In(),
+            .Data_In(Channel0),
             .Kernel0(),
             .Kernel1(),
             .Kernel2(),
@@ -24,7 +57,7 @@ module Convolution3D_3x3_stride2x2 #(parameter IMG_WIDHT = 299,parameter IMG_HEI
 
     Convolution2D_3x3_stride2x2  #(.IMG_WIDHT(IMG_WIDHT),.IMG_HEIGHT(IMG_WIDHT))
         conv2d_GREEN(
-            .Data_In(),
+            .Data_In(Channel1),
             .Kernel0(),
             .Kernel1(),
             .Kernel2(),
@@ -43,7 +76,7 @@ module Convolution3D_3x3_stride2x2 #(parameter IMG_WIDHT = 299,parameter IMG_HEI
 
     Convolution2D_3x3_stride2x2  #(.IMG_WIDHT(IMG_WIDHT),.IMG_HEIGHT(IMG_WIDHT))
         conv2d_BLUE(
-            .Data_In(),
+            .Data_In(Channel2),
             .Kernel0(),
             .Kernel1(),
             .Kernel2(),
---
+++ Project/RTL/Convolution/Kernel_3x3_stride_1x1.v
@@ -0,0 +1,5 @@
+module Kernel_3x3_stride_1x1 (
+    ports
+);
+    
+endmodule
